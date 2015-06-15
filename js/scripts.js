5958ae0a60494680d57152daae7a3923fda10300
swoods2876 <stephen.wooods@gmail.com>
2015-06-15 14:13:15 -0400

    lesson 8

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+
+
+$(document).ready(function(){
+    $("#ball").click(function(){
+        $("#demo").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#ball2").click(function(){
+        $("#demo2").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#ball3").click(function(){
+        $("#demo3").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#ball4").click(function(){
+        $("#demo4").slideDown("slow");
+    });
+});
+
