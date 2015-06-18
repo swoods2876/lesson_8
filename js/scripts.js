edbf30067cdb54c8a73f3e21fec00443194efec2
swoods2876 <stephen.wooods@gmail.com>
2015-06-18 17:26:17 -0400

    lesson8+9

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,54 @@
-
-
-
 $(document).ready(function(){
+
     $("#ball").click(function(){
         $("#demo").slideDown("slow");
     });
-});
 
-$(document).ready(function(){
     $("#ball2").click(function(){
         $("#demo2").slideDown("slow");
     });
-});
 
-$(document).ready(function(){
     $("#ball3").click(function(){
         $("#demo3").slideDown("slow");
     });
-});
 
-$(document).ready(function(){
     $("#ball4").click(function(){
         $("#demo4").slideDown("slow");
+
     });
-});
+       
+    
+
+      $(".container").click(function open(){ 
+        $('html,body').animate({
+
+
+    scrollTop: 'toggle', 
+        scrollTop: $(".container").offset().top + 600
+}, 1000); 
+
+    scrollTop: $(".container").offset().top - 600
+}, 1000); 
+
+
+
+  
+
+      
+ }
+
+  
+
+
+
+
+    });
+
+    
+
+
+
+ 
+
+
 
